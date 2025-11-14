45aa10267b84996f8a53436894cb2e1b83dec3e5
Bereket Amare Mamo <bekiamx@gmail.com>
2025-11-14 09:19:48 +0300

    remove scroll down text from motivation comopnents

--- src/app/components/Motivation.jsx
+++ src/app/components/Motivation.jsx
@@ -3,7 +3,7 @@ import React, { useRef } from "react";
 
 function Motivation() {
   return (
-    <div className="max-w-5xl mx-auto">
+    <div className="max-w-5xl mx-auto ">
       <div className="h-[80vh] flex flex-col justify-center items-center text-center">
         <TextAnimation
           text="Creative ideas start here."
@@ -16,7 +16,7 @@ function Motivation() {
               transition: { ease: "linear" },
             },
           }}
-          classname="xl:text-8xl text-7xl max-w-md mx-auto font-medium capitalize"
+          classname="xl:text-8xl sm:text-7xl text-5xl max-w-md mx-auto font-medium capitalize"
         />
       </div>
 
@@ -25,7 +25,7 @@ function Motivation() {
           as="p"
           letterAnime={true}
           text="Let's team up and turn ideas into reality ✨"
-          classname="text-7xl max-w-md lowercase"
+          classname="sm:text-7xl text-5xl max-w-md lowercase"
           variants={{
             hidden: { filter: "blur(4px)", opacity: 0, y: 20 },
             visible: {
@@ -44,16 +44,16 @@ function Motivation() {
         <TextAnimation
           text="Turning concepts into reality"
           direction="right"
-          classname="text-7xl max-w-md ml-auto capitalize"
+          classname="sm:text-7xl text-5xl max-w-md ml-auto capitalize"
         />
       </div>
 
       <div className="h-[80vh] flex justify-center items-center text-center">
         <TextAnimation
-          text="Dream big, work hard & achieve greatness "
+          text="Dream big, work hard & let's do the work for you  "
           direction="down"
           lineAnime={true}
-          classname="text-7xl max-w-md mx-auto capitalize"
+          classname="text-7xl sm:text-5xl max-w-md mx-auto capitalize"
         />
       </div>
     </div>
